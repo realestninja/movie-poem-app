4e83928d8ce32b6aec988dd513d4078783f00561
Nikhil Nawgiri <the@realest.ninja>
2023-05-15 16:09:35 +0200

    Update loadingBar styles and add silly reasons

--- src/components/loading-bar/LoadingBar.jsx
+++ src/components/loading-bar/LoadingBar.jsx
@@ -1,9 +1,38 @@
+import { useState, useEffect } from 'react';
 import './styles/loadingBar.css'
 
-const LoadingBar = () => (
-  <div className="loading-bar">
-    <div className="loading-bar-inner"></div>
-  </div>
-);
+const arrayOfSillyReasons = [
+  "Renting it online",
+  "Watching movie in super speed (with subtitles)",
+  "Writing the poem",
+];
+
+const pickReason = timing => {
+  if (timing > 7) return arrayOfSillyReasons[0];
+  if (timing > 5) return arrayOfSillyReasons[1];
+  return arrayOfSillyReasons[2];
+}
+
+const LoadingBar = () => {
+  const [count, setCount] = useState(8);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCount(count - 1);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [count]);
+
+  return (
+    <div className="loading-wrapper">
+      <div>The AI is working hard!</div>
+      <div className="loading-bar">
+        <div className="loading-bar-inner"></div>
+      </div>
+      <div className="silly-reasons">{pickReason(count)}</div>
+    </div>
+  );
+};
 
 export default LoadingBar;
