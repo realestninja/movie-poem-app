5d2c53bd664e6ca994a6f9a4f5b78b087b8fc4df
Nikhil Nawgiri <the@realest.ninja>
2023-05-15 18:06:30 +0200

    Add UserSettings component

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,23 @@ import { useEffect, useState } from "react";
 import './App.css'
 import LoadingBar from "./components/loading-bar/LoadingBar";
 import PoemDisplay from "./components/poem-display/PoemDisplay";
+import UserSettings from "./components/user-settings/UserSettings";
 import Search from './components/search/Search'
 import SearchResults from "./components/search/SearchResults";
 import { fetchPoemFromApi } from "./helper/poemApiHelper";
 
 function App() {
+  const [userSettings, setUserSettings] = useState({})
   const [searchResults, setSearchResults]  = useState([]);
   const [itemChoiceByUser, setItemChoiceByUser]  = useState(null);
   const [generatedPoem, setGeneratedPoem] = useState("");
+
   const poemGenerationAbortController = new AbortController();
   const { signal } = poemGenerationAbortController;
 
   useEffect(() => {
     console.log("itemChoiceByUser:", itemChoiceByUser);
+    console.log("userSettings:", userSettings);
 
     if(itemChoiceByUser && !generatedPoem.length) {
       fetchPoemFromApi({
@@ -24,7 +28,7 @@ function App() {
         signal,
       });
     }
-  }, [itemChoiceByUser, signal, generatedPoem]);
+  }, [itemChoiceByUser, signal, generatedPoem, userSettings]);
 
   useEffect(() => {
     console.log("generatedPoem:", generatedPoem);
@@ -41,13 +45,17 @@ function App() {
     <>
       <h1 onClick={resetApp}>movie poem app</h1>
       {!itemChoiceByUser && (
-        <>
+        <div className="user-interaction-area">
+          <UserSettings
+            setter={setUserSettings}
+            userSettings={userSettings}
+          />
           <Search setSearchResults={setSearchResults} />
           <SearchResults
             items={searchResults}
             pickItem={setItemChoiceByUser}
           />
-        </>
+        </div>
       )}
       {itemChoiceByUser && generatedPoem.length === 0 && <LoadingBar />}
       {generatedPoem.length > 0 && <PoemDisplay content={generatedPoem} />}
---
+++ src/components/user-settings/UserSettings.jsx
@@ -0,0 +1,47 @@
+import PropTypes from "prop-types";
+import noop from "lodash/noop";
+
+import "./styles/userSettings.css";
+
+function UserSettings({ setter, userSettings }) {
+  const { mood = "", language = "en" } = userSettings;
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setter({ [name]: value });
+  };
+
+  return (
+    <div className="user-settings">
+      <div>
+        <label>Mood</label>
+        <select name="mood" value={mood} onChange={handleChange}>
+          <option value="">Default</option>
+          <option value="cheerful">Cheerful</option>
+          <option value="gloomy">Gloomy</option>
+        </select>
+      </div>
+      <div>
+        <label>Language</label>
+        <select name="language" value={language} onChange={handleChange}>
+          <option value="english">English</option>
+          <option value="german">German</option>
+          <option value="french">French</option>
+          <option value="spanish">Spanish</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+UserSettings.propTypes = {
+  setter: PropTypes.func,
+  userSettings: PropTypes.object
+};
+
+UserSettings.defaultProps = {
+  setter: noop,
+  userSettings: {}
+};
+
+export default UserSettings;
