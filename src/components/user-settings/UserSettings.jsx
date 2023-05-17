acf8f266af68a374ccb01c7424b62c39957e5b5f
Nikhil Nawgiri <the@realest.ninja>
2023-05-17 09:58:53 +0200

    Refactor: Rename mood and language settings

--- src/components/user-settings/UserSettings.jsx
+++ src/components/user-settings/UserSettings.jsx
@@ -4,7 +4,7 @@ import noop from "lodash/noop";
 import "./styles/userSettings.css";
 
 function UserSettings({ setter, userSettings }) {
-  const { mood = "", language = "en" } = userSettings;
+  const { poemStyle = "", poemLanguage = "en" } = userSettings;
 
   const handleChange = event => {
     const { name, value } = event.target;
@@ -16,8 +16,8 @@ function UserSettings({ setter, userSettings }) {
   return (
     <div className="user-settings">
       <div>
-        <label>Mood</label>
-        <select name="mood" value={mood} onChange={handleChange}>
+        <label>Style</label>
+        <select name="poemStyle" value={poemStyle} onChange={handleChange}>
           <option value="">Default</option>
           <option value="cheerful">Cheerful</option>
           <option value="gloomy">Gloomy</option>
@@ -25,7 +25,7 @@ function UserSettings({ setter, userSettings }) {
       </div>
       <div>
         <label>Language</label>
-        <select name="language" value={language} onChange={handleChange}>
+        <select name="poemLanguage" value={poemLanguage} onChange={handleChange}>
           <option value="english">English</option>
           <option value="serbian">Serbian</option>
           <option value="german">German</option>
